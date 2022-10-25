5fa9c7043bea13061eaeee7bec9e471a60ccc869
Nestor <nestoraorlando@gmail.com>
2022-10-25 00:58:34 -0300

    Se crea M V T, funciona a medias

--- About/models.py
+++ About/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Administrador(models.Model):
+    nombre = models.CharField(max_length = 100)
+    apellido = models.CharField(max_length = 100)
+    edad = models.IntegerField()
+    profesion = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return f"Nombre {self.nombre}, Apellido {self.apellido}, Edad {self.edad}, profesión {self.profesion}"
--- About/views.py
+++ About/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+from About.models import Administrador
+
 
 # Create your views here.
+
+class ListaPostulantes(View):
+    template_name = "About/lista_de_postulantes.html/"
+
+    def get(self, request):
+        postulantes = Administrador.objects.all()
+        return render(request, self.template_name, {"postulantes":postulantes})
+
